356d6f80ca1f9a0783ebd35f0e16aa358244af33
jefferymoulton <jeff@jefferymoulton.com>
2021-05-15 20:41:05 -0700

    Reject duplicate email addresses for accounts.

--- src/main/java/com/writerxl/api/controller/UserController.java
+++ src/main/java/com/writerxl/api/controller/UserController.java
@@ -3,11 +3,14 @@ package com.writerxl.api.controller;
 import com.writerxl.api.model.User;
 import com.writerxl.api.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/user")
